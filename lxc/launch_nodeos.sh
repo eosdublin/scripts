6238434d16eaf49e243c2eaa00ff45c5f009d847
Sam Noble <sam@eosdublin.com>
2018-05-28 23:37:24 +0200

    Renamed variables for clarity. Added check to rename config files if they’re not correctly named.

--- lxc/launch_nodeos.sh
+++ lxc/launch_nodeos.sh
@@ -14,19 +14,24 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # </Imports>
 
 # <Parameters>
-NODEOS_LXC_TEMPLATE=$1
+EOS_LXC_TEMPLATE=$1
 CONTAINER_NAME=$2
-NODEOS_CONFIG=$3
-EOS_CONFIG=$4
+EOS_CONFIG=$3
+SHELL_CONFIG=$4
 # </Parameters>
+
+# <Locals>
+CONFIG_INI_NAME=$(basename $EOS_CONFIG)
+SHELL_CONFIG_NAME=$(basename $SHELL_CONFIG)
+# </Locals>
 
 # <Body>
 
-NODEOS_PORT=$(grep p2p-server-address= $NODEOS_CONFIG | cut -d':' -f2)
+NODEOS_PORT=$(grep p2p-server-address= $EOS_CONFIG | cut -d':' -f2)
 
 echo ">>> Launching nodeos container..."
 
-if ! . launch.sh $NODEOS_LXC_TEMPLATE $CONTAINER_NAME $NODEOS_PORT $NODEOS_PORT
+if ! . launch.sh $EOS_LXC_TEMPLATE $CONTAINER_NAME $NODEOS_PORT $NODEOS_PORT
 then
 	echo ">>> Error launching nodeos container."
 	exit 1
@@ -34,18 +39,36 @@ fi
 
 echo ">>> Pushing nodeos config.ini..."
 
-if ! lxc file push -rp "$NODEOS_CONFIG" $CONTAINER_NAME/home/eos/config/
+if ! lxc file push -rp "$EOS_CONFIG" $CONTAINER_NAME/home/eos/config/
 then
 	echo ">>> Error pushing nodeos configuration."
 	exit 1
 fi
 
+if ! [ "$CONFIG_INI_NAME" = "config.ini" ]
+then
+    if ! lxc exec $CONTAINER_NAME -- mv /home/eos/config/$CONFIG_INI_NAME /home/eos/config/config.ini
+    then
+        echo ">>> Failed to rename $CONFIG_INI_NAME to config.ini"
+        exit 1
+    fi
+fi
+
 echo ">>> Pushing EOS scripting config..."
 
-if ! lxc file push -rp "$EOS_CONFIG" $CONTAINER_NAME/home/eos/scripts/eos
+if ! lxc file push -rp "$SHELL_CONFIG" $CONTAINER_NAME/home/eos/scripts/eos
 then
 	echo ">>> Error pushing EOS scripting configuration."
 	exit 1
+fi
+
+if ! [ "$SHELL_CONFIG_NAME" = "config.sh" ]
+then
+    if ! lxc exec $CONTAINER_NAME -- mv /home/eos/scripts/eos/$SHELL_CONFIG_NAME /home/eos/scripts/eos/config.sh
+    then
+        echo ">>> Failed to rename $SHELL_CONFIG_NAME to config.sh"
+        exit 1
+    fi
 fi
 
 # Enable keosd in monit
